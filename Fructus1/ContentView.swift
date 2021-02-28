ea9dbed2b821ac04fd6c668feba118bea7f66320
simonw101 <simonhanwilson@hotmail.co.uk>
2021-02-28 16:49:15 +0000

    Initial Commit

---
+++ Fructus1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fructus1
+//
+//  Created by Simon Wilson on 28/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fructus1/Fructus1App.swift
@@ -0,0 +1,17 @@
+//
+//  Fructus1App.swift
+//  Fructus1
+//
+//  Created by Simon Wilson on 28/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct Fructus1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
